59847fc07ef598a45eacf58b0aa1b763b4ea2ba0
Liam Merz Hoffmeister <lime4992@colorado.edu>
2019-11-30 16:43:16 -0700

    scaffolding out real pose calculator class

--- ComputerVision/realPoseFromPixels.py
+++ ComputerVision/realPoseFromPixels.py
@@ -1,6 +1,18 @@
+from imutils import paths
+import numpy as np
+import imutils
 import cv2
 
 class FindWorldPoseFromPixels:
-    def __init__(self):
+    def __init__(self, image):
+        self.image = image
         self.upperleft = []
-        self.lowerRight
+    
+    def find_corner(self):
+        pass
+
+    def calibrate_camera(self):
+        pass
+
+    def calc_distance(self):
+        pass
