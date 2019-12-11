e24f280d2506abc34ebf7628ec4cccd46a0b1912
LiamMZ <lime4992@colorado.edu>
2019-12-11 14:38:33 -0700

    updating realPoseFromPixels file

--- ComputerVision/realPoseFromPixels.py
+++ ComputerVision/realPoseFromPixels.py
@@ -8,7 +8,7 @@ class FindWorldPoseFromPixels:
     def __init__(self, image):
         self.image = image
         self.upperleft = [0,0]
-        self.lowerRight = [1,1]
+        self.lowerRight = [.75,.75]
         self.calibrate_camera()
 
     
@@ -16,9 +16,11 @@ class FindWorldPoseFromPixels:
         img = self.image
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         gray = np.float32(gray)
-        dst = cv2.cornerHarris(gray,5,3,0.03)
+        dst = cv2.cornerHarris(gray,5,3,0.04)
         ret, dst = cv2.threshold(dst,0.1*dst.max(),255,0) #sets corners to white sets everything else to black
-        dst = np.uint8(dst) 
+        dst = np.uint8(dst)
+        img[dst>0.1*dst.max()]=[0,0,255]
+        cv2.imshow('image', img)
         ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
         criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
         corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
@@ -26,6 +28,8 @@ class FindWorldPoseFromPixels:
         sumCrn = [x[0]+x[1] for x in corners]
         indexMin = sumCrn.index(min(sumCrn))
         indexMax = sumCrn.index(max(sumCrn))
+        cv2.waitKey(0)
+        cv2.destroyAllWindows
         return corners[indexMin], corners[indexMax]
         
 
